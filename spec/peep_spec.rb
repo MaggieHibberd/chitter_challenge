ca63aad98a664e6955419050b7dc10fbba1a0cff
Maggie Hibberd <maggiehibberd@gmail.com>
2021-02-08 15:53:17 +0000

    test

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,15 +3,18 @@ require 'peep'
 describe Peep do 
   describe '.all' do 
     it 'returns all of the peeps' do 
-      connection = PG.connect(dbname: 'chitter_manager_test')
+      # connection = PG.connect(dbname: 'chitter_manager_test')
 
       # below may be of use when we add users
       # user1 = User.create(email: "maggieh@gmail.com", password: "oioi")
 
     
-      connection.exec("INSERT INTO peeps (content, user_id, timestamp) VALUES ('Hello Arnie', '1', '2021-02-02 13:06:07.980079');")
-      connection.exec("INSERT INTO peeps (content, user_id, timestamp)  VALUES ('Get to the chopper', '1', '2021-02-02 13:06:55.945688');")
-      connection.exec("INSERT INTO peeps (content, user_id, timestamp)   VALUES ('Aint got time to bleed', '2', '2021-02-02 13:07:07.876132');")
+      # connection.exec("INSERT INTO peeps (content, user_id, timestamp) VALUES ('Hello Arnie', '1', '2021-02-02 13:06:07.980079');")
+      # connection.exec("INSERT INTO peeps (content, user_id, timestamp)  VALUES ('Get to the chopper', '1', '2021-02-02 13:06:55.945688');")
+      # connection.exec("INSERT INTO peeps (content, user_id, timestamp)   VALUES ('Aint got time to bleed', '2', '2021-02-02 13:07:07.876132');")
+      Peep.create("INSERT INTO peeps (, , ) VALUES (content: 'Hello Arnie', user_id: '1', timestamp:'2021-02-02 13:06:07.980079');")
+      # Peep.create("INSERT INTO peeps (content, user_id, timestamp)  VALUES ('Get to the chopper', '1', '2021-02-02 13:06:55.945688');")
+      Peep.create("INSERT INTO peeps (content, user_id, timestamp)   VALUES ('Aint got time to bleed', '2', '2021-02-02 13:07:07.876132');")
 
       peep = Peep.all  
 
