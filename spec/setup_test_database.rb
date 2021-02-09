04ce5324ec4f539d8e2a51eb8efe7516e51f5a6c
Otis <otisvickersgraver@gmail.com>
2021-02-09 19:15:50 +0000

    Update to truncate users table

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,7 +3,7 @@ require 'pg'
 def setup_test_database
   p "*** Setting up test database ***"
   connection = PG.connect(dbname: 'chitter_manager_test')
-  connection.exec("TRUNCATE peeps;")
+  connection.exec("TRUNCATE peeps, users;")
 end
 
 
